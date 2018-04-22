5ab1b5e15d919b15132e36b987d61444245bca08
jueguevaramo <jueguevaramo@unal.edu.co>
2018-04-22 11:38:01 -0500

    Add files via upload

--- Basics.py
+++ Basics.py
@@ -8,56 +8,79 @@ from dipy.align.reslice import reslice
 from dipy.segment.mask import median_otsu
 from dipy.denoise.nlmeans import nlmeans
 import dipy.reconst.dti as dti
+from dipy.reconst.dti import color_fa, fractional_anisotropy, quantize_evecs
+from dipy.data import get_sphere
+from dipy.tracking.eudx import EuDX
 
-def data(ImgPath):
-    img=nib.load(ImgPath) #Muchas veces get_data
-    return img.get_data()
+def resli(ImgPath,keep=False,newzooms=(2.,2.,2.)): #Corregir newzooms entrada
+    img=nib.load(ImgPath)
+    data=img.get_data()
+    affine=img.affine
+    zooms=img.header.get_zooms()[:3] #poner condicion de zooms 
+    data, affine = reslice(data, affine, zooms,newzooms)
+    if keep:
+        nib.save(nib.Nifti1Image(data, affine),"Reslice")
+    return data, affine
+
+def otsu(data,affine,median=4,pas=4,keep=False): #Corregir median, pas entrada 
+    b0_mask, mask = median_otsu(data,median,pas)
+    if keep:
+        nib.save(nib.Nifti1Image(b0_mask.astype(np.float32), affine),"OtsuBoMask")
+        nib.save(nib.Nifti1Image(mask.astype(np.float32), affine),"OtsuMask")
+    return b0_mask, mask
+
+def Nonlocal(data,affine,keep=False,filt=26): #Preguntar! #No usan denoise images PCA
+    mask = data[..., 1] > filt
+    data2 = data #Preguntar
+    sigma = np.std(data2[~mask])
+    den = nlmeans(data2, sigma=sigma, mask=mask)
+    if keep:    
+        nib.save(nib.Nifti1Image(den.astype(np.float32),affine),"Nonlocal")
+    return den
 
 def gtab(fbvalPath,fbvecPath):
     bvals, bvecs = read_bvals_bvecs(fbvalPath, fbvecPath)
     return (gradient_table(bvals, bvecs))
 
-def resli(ImgPath):
-    img=nib.load(ImgPath)
-    data=img.get_data()
-    affine=img.affine
-    zooms=img.header.get_zooms()[:3]
-    newzooms=(2.,2.,2.)
-    data, affine = reslice(data, affine, zooms,newzooms)
-    nib.save(nib.Nifti1Image(data, affine),"Reslice")
-    return print("Reslice hecho")
-
-def otsu(ImgPath):
-    img = nib.load(ImgPath)
-    data = img.get_data()
-    b0_mask, mask = median_otsu(data, 3, 3)
-    nib.save(nib.Nifti1Image(b0_mask.astype(np.float32), img.get_affine()),"OtsuBoMask")
-    nib.save(nib.Nifti1Image(mask.astype(np.float32), img.get_affine()),"OtsuMask")
-    return print("Otsu hecho")
-
-def Nonlocal(ImgPath): #Preguntar! #No usan denoise images PCA
-    img = nib.load(ImgPath)
-    data = img.get_data()
-    mask = data[..., 1] > 49
-    data2 = data #Preguntar
-    sigma = np.std(data2[~mask])
-    den = nlmeans(data2, sigma=sigma, mask=mask)
-    nib.save(nib.Nifti1Image(den.astype(np.float32), img.get_affine()),"Nonlocal")
-    return print("Non local hecho")
-
-
-def DTImodel(ImgPath,ImgMaskPath,bvalsPath,bvecsPath):
-    img = nib.load(ImgPath)
-    data = img.get_data()
-    mask = nib.load(ImgMaskPath)
-    mask = mask.get_data()
-    bvals, bvecs = read_bvals_bvecs(bvalsPath,bvecsPath)
-    gtab = gradient_table(bvals, bvecs)
+def DTImodel(data,affine,mask,gtab,keep=False):
     tenmodel = dti.TensorModel(gtab)
     tenfit = tenmodel.fit(data, mask)
-    evecs_img = nib.Nifti1Image(tenfit.evecs.astype(np.float32), img.get_affine())
-    evals_img = nib.Nifti1Image(tenfit.evals.astype(np.float32), img.get_affine())
-    nib.save(evecs_img, "evecs")
-    nib.save(evals_img, "evals")
+    if keep:
+        evecs_img = nib.Nifti1Image(tenfit.evecs.astype(np.float32), affine)
+        evals_img = nib.Nifti1Image(tenfit.evals.astype(np.float32), affine)
+        nib.save(evecs_img, "evecs")
+        nib.save(evals_img, "evals")
+    return tenfit.evals,tenfit.evecs
 
-    return print("DTI model is done")
+def DTImaps(ImgPath,Bvalpath,Bvecpath,tracto=True):
+    data, affine=resli(ImgPath,keep=True)
+    b0_mask, mask=otsu(data,affine,keep=True)  #maask binary 
+    non= Nonlocal(b0_mask,affine,keep=True)
+    evals,evecs=DTImodel(non,affine,mask,gtab(Bvalpath,Bvecpath))
+    print('--> Calculando el mapa de anisotropia fraccional')
+    FA = fractional_anisotropy(evals)
+    FA[np.isnan(FA)] = 0
+    nib.save(nib.Nifti1Image(FA.astype(np.float32), affine),"Mapa_anisotropia_fraccional")
+    print('--> Calculando el mapa de anisotropia fraccional RGB')
+    FA2 = np.clip(FA, 0, 1)
+    RGB = color_fa(FA2, evecs)
+    nib.save(nib.Nifti1Image(np.array(255 * RGB, 'uint8'), affine),"Mapa_anisotropia_fraccional RGB")
+    print('--> Calculando el mapa de difusividad media')
+    MD1 = dti.mean_diffusivity(evals)
+    nib.save(nib.Nifti1Image(MD1.astype(np.float32), affine),"Mapa_difusividad_media")
+    if tracto:
+        sphere = get_sphere('symmetric724')
+        peak_indices = quantize_evecs(evecs, sphere.vertices)
+
+        eu = EuDX(FA.astype('f8'), peak_indices, seeds=500000, odf_vertices = sphere.vertices, a_low=0.15)
+        tensor_streamlines = [streamline for streamline in eu]
+        new_vox_sz = (2.,2.,2.)
+        hdr = nib.trackvis.empty_header()
+        hdr['voxel_size'] = new_vox_sz
+        hdr['voxel_order'] = 'LAS'
+        hdr['dim'] = FA.shape
+
+        tensor_streamlines_trk = ((sl, None, None) for sl in tensor_streamlines)
+        ten_sl_fname = "Tracto.trk"
+        nib.trackvis.write(ten_sl_fname, tensor_streamlines_trk, hdr, points_space='voxel')
+    return ("Done")
--- run.py
+++ run.py
@@ -1,14 +1,3 @@
 from Paths import *
-from Basics import data
-from Basics import otsu
-from Basics import resli
-from Basics import Nonlocal
-from Basics import DTImodel
-from Basics import gtab
-
-resli(In.img)
-Nonlocal("/home/jueguevaramo/Escritorio/Tractos/Reslice.nii")
-otsu("/home/jueguevaramo/Escritorio/Tractos/Nonlocal.nii")
-Imgpath="/home/jueguevaramo/Escritorio/Tractos/OtsuBoMask.nii"
-Imgmask="/home/jueguevaramo/Escritorio/Tractos/OtsuMask.nii"
-DTImodel(Imgpath,Imgmask,In.bval,In.bvec)
+from Basics import DTImaps
+DTImaps(In.img,In.bval,In.bvec)
